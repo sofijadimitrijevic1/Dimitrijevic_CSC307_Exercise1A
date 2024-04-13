8a5d30befbe1a0a5584bbb4939c086d9eed58d12
Sofija Dimitrijevic <dimitrij@calpoly.edu>
2024-04-12 23:21:49 -0700

    "link backend and frontend"

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
   
@@ -13,8 +13,30 @@ import Form from "./Form";
         setCharacters(updated);
     }
 
-    function updateList(person) {
-        setCharacters([...characters, person]);
+    function updateList(person) { 
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        })
+  }
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => { console.log(error); });
+    }, [] );
+
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      });
+  
+      return promise;
     }
 
     return (
@@ -28,4 +50,8 @@ import Form from "./Form";
     );
   }
 
+function fetchUsers() {
+  const promise = fetch("http://localhost:8000/users");
+  return promise;
+}
 export default MyApp;
