d3dc637cdc94bc12611c9b9aab85df69113ea5cf
Sofija Dimitrijevic <dimitrij@calpoly.edu>
2024-04-25 13:51:02 -0700

    finished exercise 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,7 +1,7 @@
 // backend.js
 import express from "express";
 import cors from "cors";
-
+import Users from "./csc-307-db-main/models/user-services.js";
 const app = express();
 const port = 8000;
 
@@ -16,105 +16,44 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-  };
-
-const findUserByJob = (job) => {
-    return users["users_list"].filter(
-        (user) => user["job"] === job
-    );
-};
-
-const deleteUserById = (id) => {
-  if (!findUserById(id)) {
-    return 404
-  }
-  users["users_list"] = users["users_list"].filter((user) => user.id != id)
-  return 200
-}
-
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name != undefined & (job != undefined)) {
-    let result = findUserByName(name) && findUserByJob(job)
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
+  Users.getUsers(name, job)
+    .then((users) => {
+      res.json({ users_list: users });
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
 });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  Users.findUserById(id)
+    .then((result) => {
+      if (result) res.send(result);
+      else res.status(404).send(`Not Found: ${id}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error.name);
+    });
 });
 
 app.delete("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
-    const deleted = deleteUserById(id)
-    if (deleted) {
-      res.status(204).send("User successfully deleted.");
-    } else {
-      res.status(404).send("User not found.");
-    }
-  });
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const generateId = () => Math.random().toFixed(8)
+  const id = req.params["id"]; //or req.params.id
+  Users.deleteUser(id)
+    .then((deleted) => {
+      if (deleted) res.status(204).send();
+      else res.status(404).send("User not found.");
+    })
+    .catch((error) => {
+      console.log("what is happening" + error);
+      res.status(500).send(error.name);
+    });
+});
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  userToAdd.id = generateId()
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  Users.addUser(userToAdd).then((result) => res.status(201).send(result));
 });
---
+++ packages/express-backend/csc-307-db-main/README.md
@@ -0,0 +1,2 @@
+# csc-307-db
+Example database for csc 307
